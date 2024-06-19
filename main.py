ef84987eb8da0b30a95251c1a1713d190720db9c
Thiago Vinicius <127901647+thiagoclopes@users.noreply.github.com>
2024-06-19 19:48:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from IPython.display import clear_output
 from predict import predict
 import tensorflow as tf
 import sys
+import serial
+import struct
 
 CHUNK = 2048
 FORMAT = pyaudio.paInt16
@@ -14,6 +16,17 @@ RATE = 44100
 THRESHOLD = 350000
 RECORD_SECONDS = 2
 
+#ser = serial.Serial('COM5', 9600, timeout=0.2)
+#if not ser.isOpen():
+#    ser.open()
+
+
+#def posicao_braco(ser, angle_base, angle_garra):
+#    # ser.write(struct.pack('BBBB', angle_base, angle_garra, angle_haste1, angle_haste2))
+#    ser.write(struct.pack('BB', angle_base, angle_garra))
+def posicao_braco(angle_base, angle_garra):
+    print(f'angulo da base:{angle_base}')
+    print(f'angulo da garra:{angle_garra}')
 
 def detecta_atividade_voz():
     global start_time
@@ -57,12 +70,21 @@ def detecta_atividade_voz():
 
 
 model = tf.keras.models.load_model('modelv2.keras')
-
+angle_base = 0
+angle_garra = 10
 if __name__ == '__main__':
     for atividade in detecta_atividade_voz():
         command = predict(model, atividade, 2, CHANNELS, RATE)
         print(command)
         # print(atividade)
-        if command == "close":
-            print("Encerrando o programa...")
-            sys.exit()
+        if command == "open":
+            angle_garra = 120
+        elif command == "close":
+            angle_garra = 10
+        elif command == "right":
+            angle_base = 180
+        elif command == "left":
+            angle_base = 0
+
+        #posicao_braco(ser, angle_base, angle_garra)
+        posicao_braco(angle_base, angle_garra)
